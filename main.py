cd5aec923881bce8ea0532691337493fb201a2c4
masoud mahdavi <masoudmahdavi1993@gmail.com>
2022-12-19 14:08:23 +0330

    update main.py in branch iss55

--- main.py
+++ main.py
@@ -16,3 +16,14 @@ first_add
 print("masoud mahdavi")
 print("mahda amini")
 print("hello qatar")
+
+
+class Human:
+    def __init__(self, color, tall, weigh, age):
+        self.color = color
+        self.tall = tall
+        self.weigh = weigh
+        self.age = age
+        
+    def __str__(self):
+        return f"{self.color} {self} {self.age}"
