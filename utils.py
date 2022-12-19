647761f787bda2721be3d2f41eda2c515ee44b9f
masoud mahdavi <masoudmahdavi1993@gmail.com>
2022-12-19 13:51:31 +0330

    update utils

--- utils.py
+++ utils.py
@@ -6,3 +6,11 @@ def sub():
     jls_extract_var = """"""
     jls_extract_var
     pass
+
+class Human:
+    def __init__(self, color, tall, weigh, age):
+        self.color = color
+        self.tall = tall
+        self.weigh = weigh
+        self.age = age
+        
